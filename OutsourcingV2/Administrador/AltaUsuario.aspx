509c49a2ec9fdd39b85d038de17d62b95eefae96
Abraham Castillo <abraham.tavella@gmail.com>
2019-11-11 19:32:53 -0600

    Cambios alta usuario

--- OutsourcingV2/Administrador/AltaUsuario.aspx
+++ OutsourcingV2/Administrador/AltaUsuario.aspx
@@ -2,52 +2,84 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h2 class="center"> Alta de usuario </h2>
-    <br />
-    Buscar persona
-    <br /><br />
-    <label> RFC </label>
-    <asp:TextBox ID="txtRFC" runat="server"></asp:TextBox>
-    <asp:Button ID="btnbuscar" runat="server" Text="Buscar" />
-    <br /><br />
-    Añadir persona
-    <br /><br />
-     <label> A. Paterno </label>
-     <asp:TextBox ID="txtAPaterno" runat="server"></asp:TextBox>
-     <label>A. Materno  </label>
-     <asp:TextBox ID="txtAMaterno" runat="server"></asp:TextBox><br /><br />
-     <label> Nombre </label>
-     <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox><br /><br />
-     <label> Calle </label>
-     <asp:TextBox ID="txtCalle" runat="server"></asp:TextBox>
-     <label> C.P. </label>
-     <asp:TextBox ID="txtCP" runat="server"></asp:TextBox>
-    <asp:Button ID="btnbuscarcp" runat="server" Text="Buscar" />
-     <label> Colonia </label>
-     <asp:TextBox ID="txtcolonia" runat="server"></asp:TextBox><br /><br />
-     <label> Correo</label>
-     <asp:TextBox ID="txtcorreo" runat="server"></asp:TextBox>
-     <label> Telefono </label>
-     <asp:TextBox ID="txttelefono" runat="server"></asp:TextBox>
-     <label> Celular </label>
-     <asp:TextBox ID="txtcelular" runat="server"></asp:TextBox>
-    <br /><br />
-    <asp:Button ID="btnagregar" runat="server" Text="Agregar" Width="150px"/>
-    <br /><br />
-    <asp:Button ID="btncancelar" runat="server" Text="Cancelar" Width="150px" />
-    <br /><br />
-    Añadir usuario
-    <br /><br />
-    <label>Usuario</label>
-    <asp:TextBox ID="txtusuario" runat="server"></asp:TextBox><br /><br />
-    <label>contraseña</label>
-    <asp:TextBox ID="txtcontraseña" runat="server"></asp:TextBox><br /><br />
-    <label>Confirmación</label>
-    <asp:TextBox ID="txtcofirmacion" runat="server"></asp:TextBox><br /><br />
-    <label>Tipo de usuario</label>
-    <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList><br /><br />
-    <label>Adscripción</label>
-    <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList><br /><br />
-    <asp:Button ID="btndaralta" runat="server" Text="Dar alta" /><br /><br />
-    <asp:Button ID="btncancelarusuario" runat="server" Text="cancelar" />
+    
+	<section id="tabs">
+	<div class="container">
+		<h6 class="section-title h1 text-center ">Alta de usuario</h6>
+		<div class="row">
+			<div class="col-xs-12 ">
+				<nav>
+					<div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+						<a class="nav-item nav-link active" id="nav-Buscar-tab" data-toggle="tab" href="#navBuscar" role="tab" aria-controls="nav-home" aria-selected="true">Buscar</a>
+						<a class="nav-item nav-link" id="nav-AgregarPersona-tab" data-toggle="tab" href="#navAgregarPersona" role="tab" aria-controls="nav-profile" aria-selected="false">Agregar Persona</a>
+						<a class="nav-item nav-link" id="nav-AgregarUsuario-tab" data-toggle="tab" href="#navAgregarUsuario" role="tab" aria-controls="nav-contact" aria-selected="false">Agregar Usuario</a>
+						<a class="nav-item nav-link" id="nav-Usuarios-tab" data-toggle="tab" href="#navUsuarios" role="tab" aria-controls="nav-about" aria-selected="false">Usuarios*</a>
+					</div>
+				</nav>
+				<div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+					<div class="tab-pane fade show active" id="navBuscar" role="tabpanel" aria-labelledby="nav-home-tab">
+					  Buscar persona
+							<br /><br />
+							<label> RFC </label>
+							<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+							<asp:Button ID="Button1" runat="server" Text="Buscar" />
+							<br /><br />
+						</div>
+					<div class="tab-pane fade" id="navAgregarPersona" role="tabpanel" aria-labelledby="nav-profile-tab">
+							Añadir persona
+						<br /><br />
+						 <label> A. Paterno </label>
+						 <asp:TextBox ID="txtAPaterno" runat="server"></asp:TextBox>
+						 <label>A. Materno  </label>
+						 <asp:TextBox ID="txtAMaterno" runat="server"></asp:TextBox><br /><br />
+						 <label> Nombre </label>
+						 <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox><br /><br />
+						 <label> Calle </label>
+						 <asp:TextBox ID="txtCalle" runat="server"></asp:TextBox>
+						 <label> C.P. </label>
+						 <asp:TextBox ID="txtCP" runat="server"></asp:TextBox>
+						<asp:Button ID="btnbuscarcp" runat="server" Text="Buscar" />
+						 <label> Colonia </label>
+						 <asp:TextBox ID="txtcolonia" runat="server"></asp:TextBox><br /><br />
+						 <label> Correo</label>
+						 <asp:TextBox ID="txtcorreo" runat="server"></asp:TextBox>
+						 <label> Telefono </label>
+						 <asp:TextBox ID="txttelefono" runat="server"></asp:TextBox>
+						 <label> Celular </label>
+						 <asp:TextBox ID="txtcelular" runat="server"></asp:TextBox>
+						<br /><br />
+						<asp:Button ID="btnagregar" runat="server" Text="Agregar" Width="150px"/>
+						<br /><br />
+						<asp:Button ID="btncancelar" runat="server" Text="Cancelar" Width="150px" />
+						<br /><br />
+					
+						</div>
+					<div class="tab-pane fade" id="navAgregarUsuario" role="tabpanel" aria-labelledby="nav-contact-tab">
+						Añadir usuario
+						<br /><br />
+						<label>Usuario</label>
+						<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br /><br />
+						<label>contraseña</label>
+						<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br /><br />
+						<label>Confirmación</label>
+						<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br /><br />
+						<label>Tipo de usuario</label>
+						<asp:DropDownList ID="DropDownList3" runat="server"></asp:DropDownList><br /><br />
+						<label>Adscripción</label>
+						<asp:DropDownList ID="DropDownList4" runat="server"></asp:DropDownList><br /><br />
+						<asp:Button ID="Button2" runat="server" Text="Dar alta" /><br /><br />
+						<asp:Button ID="Button3" runat="server" Text="cancelar" />
+						</div>
+
+					</div>
+					<div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
+
+					</div>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+</section>
+
 </asp:Content>
--- OutsourcingV2/Administrador/AltaUsuario.aspx.designer.vb
+++ OutsourcingV2/Administrador/AltaUsuario.aspx.designer.vb
@@ -13,192 +13,192 @@ Option Explicit On
 
 Partial Public Class AltaUsuario
 
-    '''<summary>
-    '''Control txtRFC.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtRFC As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control TextBox1.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control btnbuscar.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btnbuscar As Global.System.Web.UI.WebControls.Button
+	'''<summary>
+	'''Control Button1.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control txtAPaterno.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtAPaterno As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtAPaterno.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtAPaterno As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtAMaterno.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtAMaterno As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtAMaterno.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtAMaterno As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtNombre.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtNombre As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtNombre.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtNombre As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtCalle.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtCalle As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtCalle.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtCalle As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtCP.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtCP As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtCP.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtCP As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control btnbuscarcp.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btnbuscarcp As Global.System.Web.UI.WebControls.Button
+	'''<summary>
+	'''Control btnbuscarcp.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents btnbuscarcp As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control txtcolonia.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtcolonia As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtcolonia.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtcolonia As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtcorreo.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtcorreo As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtcorreo.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtcorreo As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txttelefono.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txttelefono As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txttelefono.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txttelefono As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtcelular.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtcelular As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control txtcelular.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents txtcelular As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control btnagregar.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btnagregar As Global.System.Web.UI.WebControls.Button
+	'''<summary>
+	'''Control btnagregar.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents btnagregar As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control btncancelar.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btncancelar As Global.System.Web.UI.WebControls.Button
+	'''<summary>
+	'''Control btncancelar.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents btncancelar As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control txtusuario.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtusuario As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control TextBox2.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents TextBox2 As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtcontraseña.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtcontraseña As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control TextBox3.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents TextBox3 As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control txtcofirmacion.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents txtcofirmacion As Global.System.Web.UI.WebControls.TextBox
+	'''<summary>
+	'''Control TextBox4.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents TextBox4 As Global.System.Web.UI.WebControls.TextBox
 
-    '''<summary>
-    '''Control DropDownList1.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents DropDownList1 As Global.System.Web.UI.WebControls.DropDownList
+	'''<summary>
+	'''Control DropDownList3.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents DropDownList3 As Global.System.Web.UI.WebControls.DropDownList
 
-    '''<summary>
-    '''Control DropDownList2.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents DropDownList2 As Global.System.Web.UI.WebControls.DropDownList
+	'''<summary>
+	'''Control DropDownList4.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents DropDownList4 As Global.System.Web.UI.WebControls.DropDownList
 
-    '''<summary>
-    '''Control btndaralta.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btndaralta As Global.System.Web.UI.WebControls.Button
+	'''<summary>
+	'''Control Button2.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents Button2 As Global.System.Web.UI.WebControls.Button
 
-    '''<summary>
-    '''Control btncancelarusuario.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents btncancelarusuario As Global.System.Web.UI.WebControls.Button
+	'''<summary>
+	'''Control Button3.
+	'''</summary>
+	'''<remarks>
+	'''Campo generado automáticamente.
+	'''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+	'''</remarks>
+	Protected WithEvents Button3 As Global.System.Web.UI.WebControls.Button
 End Class
