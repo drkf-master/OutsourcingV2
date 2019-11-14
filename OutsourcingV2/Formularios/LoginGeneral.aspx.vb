6d245f650f286f1d62450abc29f1f1568b24cd1f
ACASOFT-FESACA <56944424+ACASOFT-FESACA@users.noreply.github.com>
2019-11-13 20:40:21 -0600

    ARREGLANDO LOGIN

--- OutsourcingV2/Formularios/LoginGeneral.aspx.vb
+++ OutsourcingV2/Formularios/LoginGeneral.aspx.vb
@@ -7,24 +7,36 @@
 
     Protected Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
 
-		' este codigo solo es  prueba de los webservices 
-		Dim strResultado As String = "4"
-		Dim stridadscripcion As String = "48"
+        ' este codigo solo es  prueba de los webservices 
+        Dim objOutsourcing As New WSOutsourcing.OutsourcingSoapClient
+        Dim encripta As New MD5
+        Dim strResultado As String
+        Dim stridadscripcion As String = "48"
 		Dim stridservicio As String = "10"
 		Dim strmeta As String = "45"
-		Dim strestatus As String = "1"
-		Dim objOutsourcing As New WSOutsourcing.OutsourcingSoapClient
-		If IsNumeric(strResultado) Then
-			If CInt(strResultado) > 0 Then
-				Dim dsConsultaUsuario As Data.DataSet = objOutsourcing.ConsultarDatosUsuario(strResultado)
+        Dim strestatus As String = "1"
+        Dim Usuario As String = Replace(Trim(txtNombreUsuario.Text), "'", "''")
+        Dim Password As String = encripta.calculaMD5(txtPassword.Text)
+        strResultado = objOutsourcing.ValidarUsuario(Usuario, Password)
 
-				Dim dsConsultaServicios As Data.DataSet = objOutsourcing.consultarProyectosRevision(stridadscripcion, stridadscripcion, strmeta, strestatus)
+        If IsNumeric(strResultado) Then
+            If CInt(strResultado) > 0 Then
 
-				Dim dsEstatus As Data.DataSet = objOutsourcing.consultaEstatusSiguiente(stridservicio)
-			Else
+                Dim dsConsultaUsuario As Data.DataSet = objOutsourcing.ConsultarDatosUsuario(strResultado)
 
-			End If
-		Else
-		End If
+                Dim dsConsultaServicios As Data.DataSet = objOutsourcing.consultarProyectosRevision(stridadscripcion, stridadscripcion, strmeta, strestatus)
+
+                Dim dsEstatus As Data.DataSet = objOutsourcing.consultaEstatusSiguiente(stridservicio)
+                Response.Redirect("~/PaginaPrincipal.aspx")
+            Else
+                Me.lblMensajeError.Text = "El usuario no es válido. Inténtelo de nuevo."
+            End If
+        Else
+
+        End If
 	End Sub
+
+    Protected Sub txtNombreUsuario_TextChanged(sender As Object, e As EventArgs) Handles txtNombreUsuario.TextChanged
+
+    End Sub
 End Class
